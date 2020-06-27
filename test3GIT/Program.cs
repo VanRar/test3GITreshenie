fc13a25345e6fb066f5e13e02c3272b22cb2cb6c
VanRar <vitalikf7@gmail.com>
2020-06-27 20:07:43 +0300

    Добавьте файлы проекта.

---
+++ test3GIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test3GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("master vetka");
+        }
+    }
+}
